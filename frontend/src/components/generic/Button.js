b6dfc753642c9f5e42aa2ba443358c189aeabd38
VladFalcon <ZzZ05658@gmail.com>
2020-09-23 00:28:11 +0300

    Created generic components Card and Button

---
+++ frontend/src/components/generic/Button.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Button = ({background,color, content, onClick,}) => {
+    const styles = {
+        btn : {
+            opacity : "1",
+            background: background,
+            width: "100px",
+            minHeight: "30px",
+            color: color ,
+            borderRadius : "15px"
+        },
+        p : {
+            margin : "0 auto"
+        }
+    }
+
+    return (
+
+        <button onClick={onClick}
+                style={styles.btn}>
+
+            <p style={styles.p}>{content}</p>
+
+        </button>
+    )
+}
+
+export default Button;
