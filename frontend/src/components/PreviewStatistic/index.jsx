ca4ee0da353b166c88f0a1ff3e0bc1cb4912e97b
VladFalcon <ZzZ05658@gmail.com>
2020-12-19 01:38:57 +0200

    init previewStatistics component

--- frontend/src/components/PreviewStatistic/index.jsx
+++ frontend/src/components/PreviewStatistic/index.jsx
@@ -1,9 +1,79 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import ArrowUpwardIcon from '@material-ui/icons/ArrowUpward';
+import { useTranslation } from 'react-i18next';
+import CustomButton from '../generic/CustomButton';
 
-const PreviewStatistic = props => {
-  return <div></div>;
+const useStyles = makeStyles({
+  card: {
+    width: 290,
+    height: 170,
+    borderRadius: 20
+  },
+  title: {
+    textAlign: 'left',
+    font: 'normal normal medium 18px/27px Roboto',
+    color: '#293134',
+    textTransform: 'capitalize',
+    fontSize: 18
+  },
+  subtitle: {
+    font: 'normal normal normal 12px/27px Roboto',
+    color: '#B1B4BA',
+    marginTop: -7
+  },
+  arrow: {
+    color: '#4AD584',
+    fontSize: 18
+  },
+  detailsBtn: {
+    background: '#F88B38 0% 0% no-repeat padding-box',
+    borderRadius: 8,
+    width: 100,
+    height: 30,
+    margin: '-40px 0px 0px 160px',
+    position: 'absolute'
+  }
+});
+
+const PreviewStatistics = () => {
+  const classes = useStyles();
+  const { t } = useTranslation();
+
+  return (
+    <Card className={classes.card}>
+      <CardActionArea>
+        <CardContent>
+          <Typography className={classes.title}>Загальний Дохід</Typography>
+          <Typography className={classes.subtitle}>Будинки: M-2 ID0170, ID 00177</Typography>
+          <Typography className={classes.title}>
+            $12.438K <ArrowUpwardIcon className={classes.arrow} />
+          </Typography>
+          <Typography className={classes.subtitle}>Локація 1</Typography>
+          <Typography className={classes.title}>
+            $6.135K <ArrowUpwardIcon className={classes.arrow} />
+          </Typography>
+          <Typography className={classes.subtitle}>Локація 2</Typography>
+          <CustomButton
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+              className={classes.detailsBtn}
+          >
+            {t('btnDetails')}
+          </CustomButton>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+  );
 };
 
-PreviewStatistic.propTypes = {};
-
-export default PreviewStatistic;
+export default PreviewStatistics;
--- frontend/src/pages/SignIn/index.jsx
+++ frontend/src/pages/SignIn/index.jsx
@@ -5,49 +5,51 @@ import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Checkbox from '@material-ui/core/Checkbox';
 import Link from '@material-ui/core/Link';
 import Grid from '@material-ui/core/Grid';
-import {makeStyles} from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
-import {useTranslation} from "react-i18next";
-import CustomButton from "@components/generic/CustomButton";
-import * as Yup from "yup";
-import {useFormik} from "formik";
-import Logo from "../Dashboard/Header/Logo/Logo";
-import {useDispatch, useSelector} from "react-redux";
-import {login} from "@redux/action/Auth";
-import {FormHelperText} from "@material-ui/core";
-import Auth from "@redux/selector/auth/Auth";
+import { useTranslation } from 'react-i18next';
+import CustomButton from '@components/generic/CustomButton';
+import * as Yup from 'yup';
+import { useFormik } from 'formik';
+import Logo from '../Dashboard/Header/Logo/Logo';
+import { useDispatch, useSelector } from 'react-redux';
+import { login } from '@redux/action/Auth';
+import { FormHelperText } from '@material-ui/core';
+import Auth from '@redux/selector/auth/Auth';
+import PreviewStatistics from '../../components/PreviewStatistic';
 
-const useStyles = makeStyles((theme) => ({
+
+const useStyles = makeStyles(theme => ({
   paper: {
     marginTop: theme.spacing(8),
     display: 'flex',
     flexDirection: 'column',
-    alignItems: 'center',
+    alignItems: 'center'
   },
   avatar: {
     fontSize: '35px'
   },
   form: {
     width: '100%',
-    marginTop: theme.spacing(1),
+    marginTop: theme.spacing(1)
   },
   submit: {
-    margin: theme.spacing(3, 0, 2),
-  },
+    margin: theme.spacing(3, 0, 2)
+  }
 }));
 
-const SignIn = ({history}) => {
+const SignIn = ({ history }) => {
   const classes = useStyles();
-  const {t} = useTranslation();
+  const { t } = useTranslation();
   const dispatch = useDispatch();
-  const {message} = useSelector(Auth.getAuth);
+  const { message } = useSelector(Auth.getAuth);
 
   const validationSchema = Yup.object({
     email: Yup.string().required(t('validationRequired')),
-    password: Yup.string().required(t('validationRequired')),
+    password: Yup.string().required(t('validationRequired'))
   });
 
-  const {handleChange, handleSubmit, errors, values, setFieldValue} = useFormik({
+  const { handleChange, handleSubmit, errors, values, setFieldValue } = useFormik({
     initialValues: {
       remember: false
     },
@@ -55,76 +57,77 @@ const SignIn = ({history}) => {
     validateOnBlur: false,
     validationSchema,
     onSubmit: data => {
-      dispatch(login(data,history));
+      dispatch(login(data, history));
     }
   });
 
   return (
-      <Container component="main" maxWidth="xs">
-        <CssBaseline/>
-        <div className={classes.paper}>
-          <div className={classes.avatar}>
-            <Logo/>
-          </div>
-          <form className={classes.form} noValidate>
-            <TextField
-                variant="outlined"
-                margin="normal"
-                required
-                fullWidth
-                id="email"
-                label={t('signInEmail')}
-                name="email"
-                autoComplete="email"
-                error={!!errors.email}
-                helperText={errors.email}
-                onChange={handleChange}
-            />
-            <TextField
-                variant="outlined"
-                margin="normal"
-                required
-                fullWidth
-                name="password"
-                label={t('signInPassword')}
-                type="password"
-                id="password"
-                autoComplete="current-password"
-                error={!!errors.password}
-                helperText={errors.password}
-                onChange={handleChange}
-            />
-            {!!message && (
-                <FormHelperText error={!!message}>
-                  {t('signInFail')}
-                </FormHelperText>
-            )}
-            <FormControlLabel
-                control={<Checkbox color="primary"
-                                   onChange={() => setFieldValue('remember', !values.remember)}/>}
-                label={t('signInRemember')}
-            />
-            <CustomButton
-                type="submit"
-                fullWidth
-                variant="contained"
-                color="primary"
-                className={classes.submit}
-                onClick={handleSubmit}
-            >
-              {t('signInBtn')}
-            </CustomButton>
-            <Grid container>
-              <Grid item xs>
-                <Link href="#" variant="body2">
-                  {t('signInForgot')}
-                </Link>
-              </Grid>
-            </Grid>
-          </form>
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <div className={classes.avatar}>
+          <Logo />
         </div>
-      </Container>
+        <form className={classes.form} noValidate>
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label={t('signInEmail')}
+            name="email"
+            autoComplete="email"
+            error={!!errors.email}
+            helperText={errors.email}
+            onChange={handleChange}
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label={t('signInPassword')}
+            type="password"
+            id="password"
+            autoComplete="current-password"
+            error={!!errors.password}
+            helperText={errors.password}
+            onChange={handleChange}
+          />
+          {!!message && <FormHelperText error={!!message}>{t('signInFail')}</FormHelperText>}
+          <FormControlLabel
+            control={
+              <Checkbox
+                color="primary"
+                onChange={() => setFieldValue('remember', !values.remember)}
+              />
+            }
+            label={t('signInRemember')}
+          />
+          <CustomButton
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+            onClick={handleSubmit}
+          >
+            {t('signInBtn')}
+          </CustomButton>
+          <Grid container>
+            <Grid item xs>
+              <Link href="#" variant="body2">
+                {t('signInForgot')}
+              </Link>
+            </Grid>
+          </Grid>
+        </form>
+      </div>
+      <PreviewStatistics />
+    </Container>
   );
-}
+};
 
 export default SignIn;
